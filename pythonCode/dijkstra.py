Fixed broadcasting issue with tmp in makeMultiLayerGraph

pythonCode/dijkstra.py
@@ -279,7 +279,18 @@ def createLayer(case,G,K,nAR):
             tmp[randRow,k] = np.ceil(rndm.random()*100) 
         
         # Insert the temp matrix into Graph matrix 
-        G[0:n-nAR:,nAR+1:] = tmp 
+        if(0):
+            print "G Shape: " , G.shape
+            print "G: " ,  G
+            print "tmp Shape: " , tmp.shape 
+            print tmp 
+            print "G[0:n-nAR:,nAR+1:]: " , G[0:n-nAR:,n-nAR:].shape
+            print G[0:n-nAR:,n-nAR:]
+            #print G[0:n-nAR:,:nAR+1]
+            #G[0:n-nAR:,nAR+1:] = tmp
+        G[0:n-nAR:,n-nAR:] = tmp 
+        
+        print G 
         # END CASE 3 
     return 0              
 ##########################################################

pythonCode/monteCarlo.py
@@ -1,7 +1,6 @@
-import dijksta as dk
+import hrUtils as utl 
+import dijkstra as dk
 import numpy as np
-
-
 ##########################################################
 # DSCR
 #         Init function for monte carlo script 
@@ -10,28 +9,41 @@ import numpy as np
 # RTN
 #      :: 
 ##########################################################
-def monteCarlo(choice):
-
-    if ( choice == 'K' ):
+def monteCarlo(simType,RUN,L,V,f1,f2,f3,f4):
+    
+    print simType
+    
+    if ( simType == 'K' ):
     # This simulates monte carlo for nodes K
-        # RUN mcarloK
-        # retrun : [mPPCC,mNaive,msPPCC]
-        monteCarlo() 
+        print "-"*20
+        print "Monte Carlo simulation [K]"
+        print "-"*20
 
-    elif ( chcice == 2 ):
+        mPPCC,mNaive,msPPCC = monteCarloK(RUN,L,V,f1,f2,f3,f4,simType);
+        #printAverageGains(mPPCC,mNaive,msPPCC,mcSimulation); 
+
+    elif ( simType == 'R' ):
     # Simulate monte carlo for Requeasts
-        # RUN mcarloP
-        # retrun : [mPPCC,mNaive,msPPCC]
-    elif( choice == 3 ):
+        print "-"*20
+        print "Monte Carlo simulation [R]" 
+        print "-"*20
+
+        #mPPCC,mNaive,msPPCC = monteCarloR(RUN,L,V,f1,f2,f3,f4,simType);
+        #printAverageGains(mPPCC,mNaive,msPPCC,simType); 
+
+    elif( simType == 'Rho' ):
     # Simulate Rho change in monte Carlo
         # RUN mcarloRHO
         # retrun : [mPPCC,mNaive,msPPCC]
+        print "-"*20
+        print"Monte Carlo simulation [Rho]"
+        print "-"*20
+        
+        #mPPCC,mNaive,msPPCC = monteCarloRho(RUN,L,V,f1,f2,f3,f4,simType);
+        #printAverageGains(mPPCC,mNaive,msPPCC,simType); 
+
     else:
         return 0
-
-
-
-
 ###########################################################
 # DESCR
 #         prints the average gains of monte carlo type 
@@ -96,6 +108,68 @@ def printRound(numNodes,numRequest,rhoVal,mP,mN,mS,mcSimulation):
     print "Mean AGW\t:"      , mN  
     print "Mean SPBA\t:"     , mS  
     print "Mean PPCC\t:"     , mP
+
+###########################################################    
+# Func     : monteCarloK
+# Decr     : Monte carlo simulation with a varying 
+#             topology size. Everything else remains 
+#             constant. 
+# Input    :
+#      K  :: The number of nodes for the graph
+#      G  :: The graph G to be returned
+#     nR  :: The number of access routers
+# Return   :
+#  mPPCC  :: monte c ppcc avrg sol vector for each k
+#  mNaive :: monte c naive avrg sol vector for each k
+###########################################################    
+def monteCarloK(RUN,L,V,f1,f2,f3,f4,simType): 
+    NUMBER_REQUESTS = 5
+    K_START = 10
+    K_END   = 20
+    counter = 1
+    
+    for kk in range(K_START,K_END):
+        K = kk
+        AuxVec=1
+        
+        while( AuxVec <= RUN ):
+            for i in range(1,RUN):
+                # Generate the inputs for the iteration 
+                R     = utl.makeR(NUMBER_REQUESTS)
+                U,u   = utl.makeU(K)
+                C     = utl.makeC(K,L)
+                nAR   = utl.getRandomAR()
+                D     = utl.makeD(K,nAR)
+                D,o   = utl.chooseO(D)
+                rho   = utl.makeRho(D,0.5)
+                G     = dk.generateMultiLayerGraph(K,nAR)
+                P     = utl.makeP(G)
+                
+                # Evalutate Each Algorithm with the above inputs 
+                costPPCC[i]  = hrstc.ppcc(G,K,L,R,P,V,C,D,U,u,Sr,nAR,rho,o,f1,f2,f3,f4)
+                costStatic[i]= hrstc.staticPPCC(G,K,L,R,P,V,C,D,U,u,Sr,nAR,rho,o,f1,f2,f3,f4)
+                costNaive[i] = hrstc.naive(P,D,R,rho)
+                
+                # Increase itter by one 
+                AuxVec = AuxVec+1
+                
+                if (AuxVec == RUN): 
+                    break
+                else:
+                    AuxVec = AuxVec+1
+                    break
+                # end For 
+            #end While 
+
+            mPPCC[counter] = np.mean(costPPCC)
+            msPPCC[counter]= np.mean(costStatic) 
+            mNaive[counter]= np.mean(costNaive)
+            
+            #printRound(kk,NUMBER_REQUESTS,rho,mPPCC(counter),mNaive(counter),msPPCC(counter),mcSimulation)
+            counter = counter +1      
+    # end for 
+
+
 
 
 